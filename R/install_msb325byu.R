7217d0a04134b54a568fa4fcb8d1b70dd5943986
Nile Hatch <nile@byu.edu>
2023-09-19 03:06:33 -0600

    install instructions

--- R/install_msb325byu.R
+++ R/install_msb325byu.R
@@ -1,5 +1,14 @@
-library(tidyverse)
-library(devtools)
+# LET'S TRY creating the package first and connecting to a github repository
+#It is possible to first make a package project in RStudio and then connect it to GitHub afterwards. This workflow requires that you have a Git Personal Access Token (PAT) established:
+# Click File > New Project > New Directory
+# Scroll down and select R Package
+# Select a package name and click Create Project
+# Then run:
+usethis::use_git()
+usethis::use_github() # you have to have a PAT setup
 
-install_github("nilehatch/msb325byu")
-library(msb325byu)
+getwd()
+usethis::use_tutorial("02 EDA", "Exploratory Data Analysis (EDA)", open = interactive())
+
+devtools::install()
+usethis::use_testthat()
